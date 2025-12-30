f14464ac25dae0d283e25c60cef4fa026f20e1de
Himanhipatel04 <himanshipatel0409@gmail.com>
2025-12-30 11:41:18 +0530

    first commit

---
+++ src/test/java/com/example/springThree/springThree/SpringThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springThree.springThree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
